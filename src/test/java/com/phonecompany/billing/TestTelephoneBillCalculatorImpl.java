4bfe821505403ba77f1463cae35a4f0affbd53c0
Martin Kubíček <xmartinkubicek@gmail.com>
2022-02-24 12:29:48 +0100

    Pouze prejmenovani.

--- src/test/java/com/phonecompany/billing/TestTelephoneBillCalculatorImpl.java
+++ src/test/java/com/phonecompany/billing/TestTelephoneBillCalculatorImpl.java
@@ -9,17 +9,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TelephoneBillCalculatorImplTest {
+class TestTelephoneBillCalculatorImpl {
 
     private final TelephoneBillCalculatorImpl telephoneBillCalculator;
 
-    public TelephoneBillCalculatorImplTest() {
+    public TestTelephoneBillCalculatorImpl() {
         this.telephoneBillCalculator = new TelephoneBillCalculatorImpl();
     }
 
